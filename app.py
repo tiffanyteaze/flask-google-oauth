91538ed24fe317c7be4c95d745c3ae5e253f0d82
tiffanyteaze <tteaze@gmail.com>
2019-03-31 20:23:31 -0700

    attempts to fix redirect_uri error

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
 import os
 from flask import Flask, g
-from flask import render_template, flash, redirect, url_for
+from flask import render_template, flash, redirect, url_for, session
+from flask_oauth import OAuth
 
 import json
 import models
+import functools
+import flask
 
+from authlib.client import OAuth2Session
+import google.oauth2.credentials
+import googleapiclient.discovery
+
+
+GOOGLE_CLIENT_ID = '214512520695-9om7eammcfcefjedqao6lk0erf14fpq7.apps.googleusercontent.com'
+GOOGLE_CLIENT_SECRET = 'C48_DSE_KeHnHMXhpRrPsCko'
+REDIRECT_URI = '/authorized'
+
+ACCESS_TOKEN_URI = 'https://www.googleapis.com/oauth2/v4/token'
+AUTHORIZATION_URL = 'https://accounts.google.com/o/oauth2/v2/auth?access_type=offline&prompt=consent'
+AUTHORIZATION_SCOPE ='openid email profile'
+
+AUTH_REDIRECT_URI = os.environ.get("REDIRECT_URI", default=False)
+BASE_URI = os.environ.get("http://localhost:8000", default=False)
+CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", default=False)
+CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", default=False)
+
+AUTH_TOKEN_KEY = 'auth_token'
+AUTH_STATE_KEY = 'auth_state'
+USER_INFO_KEY = 'user_info'
+
+SECRET_KEY = 'oauthsecretkey'
 DEBUG = True
 PORT = 8000
 
 template_dir = os.path.abspath('./')
 app = Flask(__name__, template_folder=template_dir)
 app.secret_key = 'adkjfalj.adflja.dfnasdf.asd'
+oauth = OAuth()
 
 @app.before_request
 def before_request():
@@ -26,7 +53,71 @@ def after_request(response):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    if is_logged_in():
+        user_info = get_user_info()
+        return 'You are currently logged in as ' + user_info['given_name']
+
+    return 'You are not currently logged in'
+
+def no_cache(view):
+    @functools.wraps(view)
+    def no_cache_impl(*args, **kwargs):
+        response = flask.make_response(view(*args, **kwargs))
+        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+        response.headers['Pragma'] = 'no-cache'
+        response.headers['Expires'] = '-1'
+        return response
+
+    return functools.update_wrapper(no_cache_impl, view)
+
+@app.route('/google/login')
+@no_cache
+def login():
+    session = OAuth2Session(CLIENT_ID, CLIENT_SECRET, scope=AUTHORIZATION_SCOPE, redirect_uri=AUTH_REDIRECT_URI)
+    uri, state = session.authorization_url(AUTHORIZATION_URL)
+    flask.session[AUTH_STATE_KEY] = state
+    flask.session.permanent = True
+    return flask.redirect(uri, code=302)
+
+@app.route('/google/auth')
+@no_cache
+def google_auth_redirect():
+    state = flask.request.args.get('state', default=None, type=None)
+    
+    session = OAuth2Session(CLIENT_ID, CLIENT_SECRET, scope=AUTHORIZATION_SCOPE, state=state, redirect_uri=AUTH_REDIRECT_URI)
+    oauth2_tokens = session.fetch_access_token(ACCESS_TOKEN_URI, authorization_response=flask.request.url)
+    flask.session[AUTH_TOKEN_KEY] = oauth2_tokens
+
+    return flask.redirect(BASE_URI, code=302)
+
+@app.route('/google/logout')
+@no_cache
+def logout():
+    flask.session.pop(AUTH_TOKEN_KEY, None)
+    flask.session.pop(AUTH_STATE_KEY, None)
+    flask.session.pop(USER_INFO_KEY, None)
+
+    return flask.redirect(BASE_URI, code=302)
+
+def is_logged_in():
+    return True if AUTH_TOKEN_KEY in flask.session else False
+
+def build_credentials():
+    if not is_logged_in():
+        raise Exception('User must be logged in')
+
+    oauth2_tokens = flask.session[AUTH_TOKEN_KEY]
+    return google.oauth2.credentials.Credentials(
+        oauth2_tokens['access_token'],
+        refresh_token=oauth2_tokens['refresh_token'],
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        token_uri=ACCESS_TOKEN_URI)
+
+def get_user_info():
+    credentials = build_credentials()
+    oauth2_client = googleapiclient.discovery.build('oauth2', 'v2', credentials=credentials)
+    return oauth2_client.userinfo().get().execute()
 
 if __name__ == '__main__':
     models.initialize()
